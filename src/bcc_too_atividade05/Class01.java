7fb33d1aa533f99e9156dd563a12ce379392c4c3
Camila Barcellos <camilaafbarcellos@gmail.com>
2021-12-13 23:57:37 -0300

    Commit inicial da tarefa, processo 03.

---
+++ src/bcc_too_atividade05/Class01.java
@@ -0,0 +1,12 @@
+package bcc_too_atividade05;
+
+/**
+ *
+ * @author Camila Barcellos <BCC-TOO IFSul>
+ */
+public class Class01 {
+    public static void main(String[] args) {
+        System.out.println("Esta é a primeira classe do projeto!");
+    }
+    
+}
---
+++ src/bcc_too_atividade05/Class03.java
@@ -0,0 +1,11 @@
+package bcc_too_atividade05;
+
+/**
+ *
+ * @author Camila Barcellos <BCC-TOO IFSul>
+ */
+public class Class03 {
+    public static void main(String[] args) {
+        System.out.println("Esta é a terceira classe do projeto!");
+    }
+}
