036684a21091acdc1bb893e008dba73b36a69cc2
Camila Barcellos <camilaafbarcellos@gmail.com>
2021-12-14 00:42:25 -0300

    Commit do arquivo da nova branch.

---
+++ src/bcc_too_atividade05/NovaBranch.java
@@ -0,0 +1,11 @@
+package bcc_too_atividade05;
+
+/**
+ *
+ * @author Camila Barcellos <BCC-TOO IFSul>
+ */
+public class NovaBranch {
+    public static void main(String[] args) {
+        System.out.println("Esta é a nova branch criada!");
+    }
+}
